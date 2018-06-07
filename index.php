e87f6fc829109fb69747fa6dd0ffaa8d0429ead6
Naomi <naomi.paulmin@ynov.com>
2018-06-07 22:41:59 +0200

    Add header / navbar

--- index.php
+++ index.php
@@ -8,8 +8,124 @@ include("includes/init.php");
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>Subskill Exercice</title>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+  <link rel="stylesheet" href="css/main.css"/>
 </head>
 <body>
-  <h1>Hello !</h1>
+  <div class="row" id="header">
+    <div class="col-md-2 logo">
+      <img src="images/logo.png" alt="logo" width="120vw" height="120vh">
+    </div>
+    <div class="col-md-10">
+      <div class="row" id="bar1">
+        <div class="col-md-4 highlighted centeredVert">
+          <p class="">Un réseau de <span>1300</span> crèches</p>
+        </div>
+        <div class="col-md-6 centeredVert" id="number">
+          <svg width="20px" height="19px" viewBox="0 0 20 19" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+            <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round">
+              <g id="10.1-D-Resultat-Recherche-Liste" transform="translate(-1159.000000, -22.000000)" stroke="#2173BA">
+                <g id="MENU">
+                  <g id="CONTACT" transform="translate(1160.000000, 23.000000)">
+                    <g id="TELEPHONE">
+                      <g id="Group">
+                            <path d="M5.46484631,4.40689479 L3.8061671,2.76973285 C3.19647352,2.16766481 2.20533062,2.16766481 1.59563704,2.76973285 L0.685786619,3.66820361 C-0.102906744,4.44703266 -0.224845461,5.66274697 0.39031973,6.58128667 C3.03232525,10.5263761 6.55604149,14.0060206 10.5518794,16.615754 C11.4804896,17.2216814 12.7147283,17.1012678 13.5010767,16.3232107 L14.4117088,15.4247399 C15.022184,14.8226719 15.022184,13.8446972 14.4117088,13.2418573 L12.7530296,11.6046954 C12.1425543,11.0018555 11.1529748,11.0018555 10.5424995,11.6046954 L9.989867,12.150416 C8.17172948,10.6792087 6.40127312,8.93166767 4.91221379,7.1362699 L5.46484631,6.59054925 C6.07453989,5.98770933 6.07453989,5.00973471 5.46484631,4.40689479 Z" id="Stroke-1" stroke-width="1.5"></path>
+                            <path d="M8.60869565,0 C13.7959622,0 18,4.15095652 18,9.27272727" id="Stroke-3"></path>
+                            <path d="M8.60869565,3.09090909 C12.0663652,3.09090909 14.8695652,5.85789091 14.8695652,9.27272727" id="Stroke-5"></path>
+                            <path d="M8.60869565,6.18181818 C10.3375901,6.18181818 11.7391304,7.56477922 11.7391304,9.27272727" id="Stroke-7"></path>
+                      </g>
+                    </g>
+                  </g>
+                </g>
+              </g>
+            </g>
+          </svg>
+          <p>01 85 53 06 93</p>
+        </div>
+        <div class="col-md-1 centeredVert" id="email">
+          <svg width="22px" height="15px" viewBox="0 0 22 15" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+            <defs>
+              <path d="M19.5454545,11.8181818 C19.5454545,12.5709091 18.9345455,13.1818182 18.1818182,13.1818182 L1.81818182,13.1818182 C1.06545455,13.1818182 0.454545455,12.5709091 0.454545455,11.8181818 L0.454545455,1.81818182 C0.454545455,1.06454545 1.06545455,0.454545455 1.81818182,0.454545455 L18.1818182,0.454545455 C18.9345455,0.454545455 19.5454545,1.06454545 19.5454545,1.81818182 L19.5454545,11.8181818 L19.5454545,11.8181818 Z" id="path-1"></path>
+            </defs>
+            <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round">
+              <g id="10.1-D-Resultat-Recherche-Liste" transform="translate(-1340.000000, -23.000000)">
+                <g id="MENU">
+                  <g id="MAIL" transform="translate(1320.000000, 0.000000)">
+                    <g id="enveloppe" transform="translate(21.000000, 24.000000)">
+                      <g id="mail">
+                        <g id="Stroke-152" stroke="#FFFFFF">
+                          <use stroke-width="1.5" fill="#FFFFFF" fill-rule="evenodd" xlink:href="#path-1"></use>
+                          <use stroke-width="1" xlink:href="#path-1"></use>
+                        </g>
+                        <polyline id="Stroke-153" stroke="#2173BA" stroke-width="1.5" points="19.5454545 0.727272727 10 8.36363636 0.454545455 0.727272727"></polyline>
+                      </g>
+                    </g>
+                  </g>
+                </g>
+              </g>
+            </g>
+          </svg>
+        </div>
+        <div class="col-md-1 centeredVert" id="profil">
+          <svg width="16px" height="22px" viewBox="0 0 16 22" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+            <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round">
+              <g id="10.1-D-Resultat-Recherche-Liste" transform="translate(-1401.000000, -19.000000)" fill="#FFFFFF">
+                <g id="MENU">
+                  <g id="contact" transform="translate(1380.000000, 0.000000)">
+                    <g id="Group-24" transform="translate(22.000000, 19.000000)">
+                      <g id="Group-8" transform="translate(0.254545, 0.654545)">
+                        <g id="user">
+                          <path d="M13.7454545,20.2181818 L0,20.2181818 C0,13.4737455 3.07554545,8 6.87272727,8 C10.6699091,8 13.7454545,13.4737455 13.7454545,20.2181818 L13.7454545,20.2181818 Z" id="Stroke-1803" stroke="#FFFFFF"></path>
+                          <path d="M6.87272727,11.0545455 C3.91776,11.0545455 1.52727273,8.66192 1.52727273,5.70909091 C1.52727273,2.75626182 3.91776,0.363636364 6.87272727,0.363636364 C9.82555636,0.363636364 12.2181818,2.75626182 12.2181818,5.70909091 C12.2181818,8.66192 9.82555636,11.0545455 6.87272727,11.0545455 Z" id="Stroke-1802" stroke="#308DDE"></path>
+                        </g>
+                      </g>
+                    </g>
+                  </g>
+                </g>
+              </g>
+            </g>
+          </svg>
+        </div>
+      </div>
+      <div class="row" id="bar2">
+        <div class="col-md-3 highlighted centeredVert" id="parent">
+          <p>Je suis un parent</p>
+          <svg id="plus" width="25px" height="25px" viewBox="0 0 25 25" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+            <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd" stroke-linejoin="round">
+              <g id="10.1-D-Resultat-Recherche-Liste" transform="translate(-498.000000, -100.000000)" stroke="#FFFFFF">
+                <g id="MENU">
+                  <g id="plus" transform="translate(499.000000, 102.000000)">
+                    <g>
+                        <path d="M23,11.5 C23,17.8526 17.8526,23 11.5,23 C5.1474,23 0,17.8526 0,11.5 C0,5.1497 5.1474,0 11.5,0 C17.8526,0 23,5.1497 23,11.5 L23,11.5 Z" id="Stroke-1866"></path>
+                        <path d="M17.25,11.5 L5.75,11.5" id="Stroke-1868" stroke-linecap="round"></path>
+                        <path d="M11.25,17.5 L11.25,5.5" id="Stroke-1868" stroke-linecap="round"></path>
+                    </g>
+                  </g>
+                </g>
+              </g>
+            </g>
+          </svg>
+        </div>
+        <div class="col-md-9 centeredVert" id="nav">
+          <ul>
+            <li> 
+              <a href="">Connaître la maison bleue</a> 
+            </li>
+            <li>|</li>
+            <li> 
+              <a href="">Découvrir nos actualités</a> 
+            </li>
+            <li>|</li>
+            <li> 
+              <a href="">Besoin d'aide</a> 
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 </body>
 </html>
